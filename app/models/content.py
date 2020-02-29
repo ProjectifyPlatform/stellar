f111fade75c25702a930839d87118f4002111882
Zeth <zeth.leonardo@protonmail.com>
2020-02-29 04:56:40 +0400

    Added comment model and relationships.

--- app/models/content.py
+++ app/models/content.py
@@ -12,17 +12,39 @@ class Project(Model):
     Projects model for storing project related details
 
     Relationships:
-    - TBA
+    - Comments
     """
+
     id = Column(db.Integer, primary_key=True)
     public_id = Column(db.String(15))
     author_id = Column(db.Integer, db.ForeignKey("user.id"))
 
+    # Relationships
+    comments = relationship("Comment", backref="project", lazy="dynamic")
+
     title = Column(db.String(255))
     content = Column(db.Text)
-
-    def get_creator_public_id(self):
-        return self.author_id.public_id
+    description = Column(db.Text)
+    created = Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return f"<Project '{self.public_id}'>"
+
+
+class Comment(Model):
+    """
+    Comments model for storing comment related details
+
+    Relationships:
+    - Comments
+    - Likes
+    """
+
+    id = Column(db.Integer, primary_key=True)
+    public_id = Column(db.String(15))
+    author_id = Column(db.Integer, db.ForeignKey("user.id"))
+    on_project = Column(db.Integer, db.ForeignKey("project.id"))
+
+    # Comment body
+    body = Column(db.Text)
+    created = Column(db.DateTime, default=datetime.utcnow)
