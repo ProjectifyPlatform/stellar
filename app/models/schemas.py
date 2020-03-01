730aaa898cdff952c553e384a76f3e45555ac6fe
Zeth <zeth.leonardo@protonmail.com>
2020-03-01 20:22:42 +0400

    Added model schemas.

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -2,7 +2,7 @@
 from app import ma
 
 from .user import User
-from .content import Project
+from .content import Project, Rating, Post, Comment, Reply
 
 
 class UserSchema(ma.ModelSchema):
@@ -18,4 +18,38 @@ class ProjectSchema(ma.ModelSchema):
         model = Project
 
         # Fields to expose.
-        fields = ("public_id", "title", "content", "description")
+        fields = (
+            "public_id",
+            "title",
+            "difficulty",
+            "time_required",
+            "abstract",
+            "objective",
+            "safety",
+            "content",
+            "created",
+        )
+
+
+class RatingSchema(ma.ModelSchema):
+    class Meta:
+        model = Rating
+
+        # Fields to expose.
+        fields = ("rating", "feedback", "rated_on")
+
+
+class PostSchema(ma.ModelSchema):
+    class Meta:
+        model = Post
+
+        # Fields to expose.
+        fields = ("public_id", "author_id", "caption", "project", "comments", "created")
+
+
+class CommentSchema(ma.ModelSchema):
+    class Meta:
+        model = Comment
+
+        # Fields to expose.
+        fields = ("body", "created", "replies")
