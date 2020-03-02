1743a5c1440e9bffe5122a5f413935f945500f01
Zeth <zeth.leonardo@protonmail.com>
2020-03-02 20:44:35 +0400

    Add project namespace.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from flask_restx import Api
 from flask import Blueprint
 
 from .user.controller import api as user_ns
+from .project.controller import api as project_ns
 
 # Import controller APIs as namespaces.
 api_bp = Blueprint("api", __name__)
@@ -10,3 +11,4 @@ api = Api(api_bp, title="API", description="Main routes.")
 
 # API namespaces
 api.add_namespace(user_ns)
+api.add_namespace(project_ns)
