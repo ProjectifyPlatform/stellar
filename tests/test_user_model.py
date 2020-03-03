34d45da72b106d7fc1c2bb1b7adfa7783a49a16c
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 20:56:44 +0400

    Added unit tests for checking roles.

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,10 +1,15 @@
-from app.models.user import User
+from app.models.user import User, Role, Permission
 from app.models.schemas import UserSchema
 
 from tests.utils.base import BaseTestCase
 
 
 class TestUserModel(BaseTestCase):
+    def setUp(self):
+        # Extend setUp
+        super(TestUserModel, self).setUp()
+        Role.insert_roles()
+
     def test_password_setter(self):
         u = User(password="cat")
         self.assertTrue(u.password_hash is not None)
@@ -18,6 +23,50 @@ class TestUserModel(BaseTestCase):
         u = User(password="penguin")
         u2 = User(password="penguin")
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_user_role(self):
+        u = User(email="test@user.com", password="gentoo")
+        r = Role.query.all()
+
+        self.assertTrue(u.has_role(Permission.FOLLOW))
+        self.assertTrue(u.has_role(Permission.COMMENT))
+        self.assertTrue(u.has_role(Permission.WRITE))
+        self.assertFalse(u.has_role(Permission.MODERATE))
+        self.assertFalse(u.has_role(Permission.CREATE))
+        self.assertFalse(u.has_role(Permission.ADMIN))
+
+    def test_creator_role(self):
+        r = Role.query.filter_by(name="Creator").first()
+        u = User(email="test@user.com", password="gentoo", role=r)
+
+        self.assertTrue(u.has_role(Permission.FOLLOW))
+        self.assertTrue(u.has_role(Permission.COMMENT))
+        self.assertTrue(u.has_role(Permission.WRITE))
+        self.assertTrue(u.has_role(Permission.CREATE))
+        self.assertFalse(u.has_role(Permission.MODERATE))
+        self.assertFalse(u.has_role(Permission.ADMIN))
+
+    def test_moderator_role(self):
+        r = Role.query.filter_by(name="Moderator").first()
+        u = User(email="john@example.com", password="cat", role=r)
+
+        self.assertTrue(u.has_role(Permission.FOLLOW))
+        self.assertTrue(u.has_role(Permission.COMMENT))
+        self.assertTrue(u.has_role(Permission.WRITE))
+        self.assertTrue(u.has_role(Permission.CREATE))
+        self.assertTrue(u.has_role(Permission.MODERATE))
+        self.assertFalse(u.has_role(Permission.ADMIN))
+
+    def test_administrator_role(self):
+        r = Role.query.filter_by(name="Admin").first()
+        u = User(email="test@user.com", password="gentoo", role=r)
+
+        self.assertTrue(u.has_role(Permission.FOLLOW))
+        self.assertTrue(u.has_role(Permission.COMMENT))
+        self.assertTrue(u.has_role(Permission.WRITE))
+        self.assertTrue(u.has_role(Permission.CREATE))
+        self.assertTrue(u.has_role(Permission.MODERATE))
+        self.assertTrue(u.has_role(Permission.ADMIN))
 
     def test_schema(self):
         u = User(username="gentoo", password="penguin")
