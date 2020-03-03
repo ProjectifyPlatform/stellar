8a236d1ff6852b77b4f309f441a7f356518bea99
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 22:26:11 +0400

    Added project deletion service method.

--- app/api/project/service.py
+++ app/api/project/service.py
@@ -2,8 +2,8 @@ from flask import current_app
 
 from app.utils import err_resp, message, internal_err_resp
 from app.models.content import Project
+from app.models.user import Permission
 
-from .utils import load_data, create_and_load
 
 
 class ProjectService:
@@ -14,6 +14,7 @@ class ProjectService:
             return err_resp("Project not found!", "project_404", 404)
 
         try:
+            from .utils import load_data
             project_data = load_data(project)
 
             resp = message(True, "Project data sent")
@@ -37,15 +38,14 @@ class ProjectService:
         content = data["content"]
 
         # Check if current_user is a creator.
-        from app.models.user import Permission
-
         if not current_user.has_role(Permission.CREATE):
             return err_resp("User is not a creator.", "user_not_creator", 403)
 
         # Create a new project
-        from uuid import uuid4
-
         try:
+            from uuid import uuid4
+            from .utils import create_and_load
+
             project = Project(
                 public_id=str(uuid4().int)[:15],
                 creator_id=current_user.id,
@@ -67,3 +67,27 @@ class ProjectService:
         except Exception as error:
             current_app.logger.error(error)
             return internal_err_resp
+
+    @staticmethod
+    def delete(public_id, current_user):
+        if not (project := Project.query.filter_by(public_id=public_id).first()):
+            return err_resp("Project not found.", "project_404", 404)
+
+        if (
+            project.creator.public_id == current_user.public_id
+            or current_user.has_role(Permission.MODERATE)
+            or current_user.has_role(Permission.ADMIN)
+        ):
+            try:
+                from .utils import delete_project
+
+                delete_project(project)
+
+                resp = message(True, "Project deleted.")
+                return resp, 200
+
+            except Exception as error:
+                current_app.logger.error(error)
+                return internal_err_resp()
+
+        return err_resp("Insufficient permissions!", "user_unauthorized", 401)
