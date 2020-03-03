b1e4f4c42bb96a622041a56d92211ff834a84a24
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 20:57:34 +0400

    Added basic create schema.

--- app/api/project/utils.py
+++ app/api/project/utils.py
@@ -7,3 +7,34 @@ def load_data(project_db_obj):
     data = project_schema.dump(project_db_obj)
 
     return data
+
+
+# Validations with Marshmallow
+from marshmallow import Schema, fields
+from marshmallow.validate import Length, Regexp, Range
+
+
+class CreateSchema(Schema):
+    """ /project/create [POST]
+
+    Parameters:
+    - Title
+    - Difficulty
+    - Time Required
+
+    -- db.Text
+    - Abstract
+    - Objective
+    - Safety
+    - Content
+    """
+
+    title = fields.Str(required=True, validate=[Length(min=2, max=128)])
+    difficulty = fields.Int(required=True, validate=[Range(min=0, max=5)])
+    time_required = fields.Str(required=True, validate=[Length(max=20)])
+
+    # Text values.
+    abstract = fields.Str(required=True)
+    objective = fields.Str(required=True)
+    safety = fields.Str()
+    content = fields.Str(required=True)
