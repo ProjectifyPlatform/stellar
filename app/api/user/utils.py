232c5d7c61a64a53724d6d09e648df53652d81bc
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 21:31:03 +0400

    Add get_user from db by id.

--- app/api/user/utils.py
+++ app/api/user/utils.py
@@ -1,3 +1,11 @@
+def get_user(id):
+    from app.models.user import User
+
+    u = User.query.get(id)
+
+    return u
+
+
 def load_data(user_db_obj):
     """ Load user's data """
     from app.models.schemas import UserSchema
