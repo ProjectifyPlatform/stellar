6019d07e72f74d6efd9a8e4b48854c643b43dcac
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 23:04:46 +0400

    Added DTO models.

--- app/api/project/dto.py
+++ app/api/project/dto.py
@@ -4,3 +4,41 @@ from flask_restx import Namespace, fields
 class ProjectDto:
 
     api = Namespace("project", description="Project related operations.")
+
+    project_obj = api.model(
+        "Project object",
+        {
+            "public_id": fields.String,
+            "title": fields.String,
+            "difficulty": fields.Integer,
+            "time_required": fields.String,
+            "abstract": fields.String,
+            "objective": fields.String,
+            "safety": fields.String,
+            "content": fields.String,
+            "created": fields.DateTime,
+        },
+    )
+
+    project_success = api.model(
+        "Project success response",
+        {
+            "success": fields.Boolean,
+            "message": fields.String,
+            "project": fields.Nested(project_obj),
+        },
+    )
+
+    project_create = api.model(
+        "Project create",
+        {
+            "title": fields.String(required=True),
+            "difficulty": fields.Integer(required=True),
+            "time_required": fields.String(required=True),
+            # Text values.
+            "abstract": fields.String(required=True),
+            "objective": fields.String(required=True),
+            "safety": fields.String,
+            "content": fields.String(required=True),
+        },
+    )
