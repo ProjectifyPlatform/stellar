250f08a25fb6a1eff50fb722be37aa533bd54570
Zeth <zeth.leonardo@protonmail.com>
2020-03-03 21:30:25 +0400

    Add project creation route service.

--- app/api/project/controller.py
+++ app/api/project/controller.py
@@ -1,7 +1,11 @@
+from flask import request
 from flask_restx import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from .service import ProjectService
 from .dto import ProjectDto
+
+from ..user.utils import get_user
 
 api = ProjectDto.api
 
@@ -15,3 +19,20 @@ class ProjectGet(Resource):
     def get(self, public_id):
         """ Get a specific project's data by its public id """
         return ProjectService.get_data(public_id)
+
+
+@api.route("/")
+class ProjectCreate(Resource):
+    @api.doc(
+        "Create a new project",
+        responses={
+            201: "Project data successfully created.",
+            403: "User is not a creator.",
+        },
+    )
+    @jwt_required
+    def post(self):
+        data = request.get_json()
+        current_user = get_user(get_jwt_identity())
+
+        return ProjectService.create(data, current_user)
