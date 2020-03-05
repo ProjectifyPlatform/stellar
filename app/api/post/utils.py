6c787e9c0479a6eb7c268764e77214647deb0c45
Zeth <zeth.leonardo@protonmail.com>
2020-03-06 00:07:21 +0400

    Removed basic utils.

--- app/api/post/utils.py
+++ app/api/post/utils.py
@@ -1,3 +1,6 @@
+from app import db
+
+
 def load_data(post_db_obj):
     """ Load post's data """
     from app.models.schemas import PostSchema
@@ -7,3 +10,21 @@ def load_data(post_db_obj):
     data = post_schema.dump(post_db_obj)
 
     return data
+
+
+def create_and_load(post_obj):
+    db.session.add(post_obj)
+    db.session.flush()
+
+    data = load_data(post_obj)
+
+    db.session.commit()
+
+    return data
+
+
+def delete_post(post_db_obj):
+    db.session.delete(post_db_obj)
+    db.session.commit()
+
+    return True
