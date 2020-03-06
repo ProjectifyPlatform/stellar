c42c002e8eb90ddc187253f272453ab4970a4029
Zeth <zeth.leonardo@protonmail.com>
2020-03-07 03:43:55 +0400

    Add CreatePost marshmallow schemas validator.

--- app/api/post/utils.py
+++ app/api/post/utils.py
@@ -28,3 +28,20 @@ def delete_post(post_db_obj):
     db.session.commit()
 
     return True
+
+
+# Validations with Marshmallow
+from marshmallow import Schema, fields
+from marshmallow.validate import Range
+
+
+class CreatePost(Schema):
+    """ /api/post/create/<string:project_public_id> [POST]
+
+    Parameters:
+    - Caption (Str)
+    - Image Hash (Str)
+    """
+
+    caption = fields.Str(required=True, validate=[Range(min=1, max=3000)])
+    image_hash = fields.Str(validate=[Range(min=40)])
