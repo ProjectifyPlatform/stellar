881c54821171714a32ac09a2af85426a07cdec33
Zeth <zeth.leonardo@protonmail.com>
2020-03-07 03:42:48 +0400

    Add post namespace.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 
 from .user.controller import api as user_ns
 from .project.controller import api as project_ns
+from .post.controller import api as post_ns
 
 # Import controller APIs as namespaces.
 api_bp = Blueprint("api", __name__)
@@ -12,3 +13,4 @@ api = Api(api_bp, title="API", description="Main routes.")
 # API namespaces
 api.add_namespace(user_ns)
 api.add_namespace(project_ns)
+api.add_namespace(post_ns)
