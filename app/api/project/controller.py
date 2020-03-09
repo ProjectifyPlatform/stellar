485f281c55a4677b94cc501bb94d628799e74ff8
Zeth <zeth.leonardo@protonmail.com>
2020-03-09 21:13:27 +0400

    Added validators and merged endpoints.

--- app/api/project/controller.py
+++ app/api/project/controller.py
@@ -7,29 +7,57 @@ from app.utils import validation_error
 # Project modules
 from .service import ProjectService
 from .dto import ProjectDto
-from .utils import CreateProject
+from .utils import CreateProject, UpdateProject
 
 from ..user.utils import get_user
 
 api = ProjectDto.api
 
 # Define models/validators.
-_project_success = ProjectDto.project_success
+_success_response = ProjectDto.success_response
 
 _create_validator = CreateProject()
+_update_validator = UpdateProject()
 
 
-@api.route("/get/<string:public_id>")
-class ProjectGet(Resource):
+@api.route("/<string:public_id>")
+class Project(Resource):
+    # Get
     @api.doc(
         responses={
-            200: ("Project data successfully sent", _project_success),
+            200: ("Project data successfully sent.", _success_response),
             404: "Project not found!",
         },
     )
     def get(self, public_id):
         """ Get a specific project's data by its public id """
         return ProjectService.get_data(public_id)
+
+    # Update
+    _update_model = ProjectDto.update_model
+
+    @api.doc(responses={200: "Project has been updated.", 404: "Project not found!",},)
+    @api.expect(_update_model, validate=True)
+    def put(self, public_id):
+        """ Update a project by its public id """
+        data = request.get_json()
+        current_user = get_user(get_jwt_identity())
+
+        # Validate data
+        if (errors := _create_validator.validate(data)) :
+            return validation_error(False, errors), 400
+
+        return ProjectService.update_data(data, public_id, current_user)
+
+    # Delete
+    @api.doc(
+        responses={200: "Project has been deleted.", 401: "Insufficient permissions."},
+    )
+    @jwt_required
+    def delete(self, public_id):
+        """ Delete a specific project from the DB using its public id """
+        current_user = get_user(get_jwt_identity())
+        return ProjectService.delete(public_id, current_user)
 
 
 @api.route("/create")
@@ -39,7 +67,7 @@ class ProjectCreate(Resource):
 
     @api.doc(
         responses={
-            201: ("Project data successfully created.", _project_success),
+            201: ("Project data successfully created.", _success_response),
             403: "User is not a creator.",
         },
     )
@@ -55,15 +83,3 @@ class ProjectCreate(Resource):
             return validation_error(False, errors), 400
 
         return ProjectService.create(data, current_user)
-
-
-@api.route("/delete/<string:public_id>")
-class ProjectDelete(Resource):
-    @api.doc(
-        responses={200: "Project has been deleted.", 401: "Insufficient permissions."},
-    )
-    @jwt_required
-    def delete(self, public_id):
-        """ Delete a specific project from the DB using its public id """
-        current_user = get_user(get_jwt_identity())
-        return ProjectService.delete(public_id, current_user)
