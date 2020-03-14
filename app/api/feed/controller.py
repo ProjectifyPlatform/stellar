52d00e65d6f70e7036f205c2a53057ef01b752e0
Zeth <zeth.leonardo@protonmail.com>
2020-03-14 20:06:20 +0400

    Add DTO response models.

--- app/api/feed/controller.py
+++ app/api/feed/controller.py
@@ -7,10 +7,16 @@ from .dto import FeedDto
 
 api = FeedDto.api
 
+# Define models
+_posts_success_response = FeedDto.posts_success_response
+_projects_success_response = FeedDto.projects_success_response
+
 
 @api.route("/posts")
 class FeedPosts(Resource):
-    # Get posts
+    @api.doc(
+        responses={200: ("Post data successfully sent.", _projects_success_response),}
+    )
     def get(self):
         page = request.args.get("page", 1, type=int)
         return FeedService.get_posts(page)
@@ -18,7 +24,9 @@ class FeedPosts(Resource):
 
 @api.route("/projects")
 class FeedProjects(Resource):
-    # Get projects
+    @api.doc(
+        responses={200: ("Post data successfully sent.", _projects_success_response),}
+    )
     def get(self):
         page = request.args.get("page", 1, type=int)
         category = request.args.get("category", None, type=str)
